9bc968250f0b84e6942cd368c86dc9e10baf34b5
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-09-07 00:06:00 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
-const button = document.querySelector('.buttons');
-const hiddenText = document.querySelector('.hidden-text');
+const questions = document.querySelectorAll(".question");
 
+// questions.forEach(function(question) {
+//     question.addEventListener("click", function(e) {
+//         const button = e.target.parentElement.parentElement;
+//         if (button.className === 'buttons') {
+//             question.classList.toggle('show-text');
+//         }
+//         questions.forEach(function(i) {
+//             if (i !== question) {
+//                 i.classList.remove('show-text');
+//             }
+//         })
+//     })
+// })
 
-button.addEventListener("click", function() {
-    hiddenText.classList.toggle('show-text');
-})
+questions.forEach(function (question) {
+  const btn = question.querySelector(".buttons");
+  btn.addEventListener("click", function () {
+    question.classList.toggle("show-text");
+    questions.forEach(function (i) {
+      if (i !== question) {
+        i.classList.remove("show-text");
+      }
+    });
+  });
+});
